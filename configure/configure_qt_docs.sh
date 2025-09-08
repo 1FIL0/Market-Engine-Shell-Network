504161767ab5b56b3dbdd9962f45ed26cbe80156
1FIL0 <serglinux2@gmail.com>
2025-09-08 17:54:01 +0100

    update

--- configure/configure_qt_docs.sh
+++ configure/configure_qt_docs.sh
@@ -5,4 +5,6 @@ UTILS_PATH=$HERE/../utils
 source $UTILS_PATH/definitions.sh
 
 pyuic5 $MARKET_ENGINE_ASSETS_PATH/ui/market_engine_client.ui -o $MARKET_ENGINE_CLIENT_APPLICATION/src/form_main_window.py
-pyrcc5 $MARKET_ENGINE_ASSETS_PATH/icons/market_engine_icons.qrc -o $MARKET_ENGINE_CLIENT_APPLICATION/src/market_engine_icons_rc.py
+pyrcc5 $MARKET_ENGINE_ASSETS_PATH/qrc/market_engine_icons.qrc -o $MARKET_ENGINE_CLIENT_APPLICATION/src/market_engine_icons_rc.py
+pyrcc5 $MARKET_ENGINE_ASSETS_PATH/qrc/brand_icons.qrc -o $MARKET_ENGINE_CLIENT_APPLICATION/src/brand_icons_rc.py
+pyrcc5 $MARKET_ENGINE_ASSETS_PATH/qrc/third_party_icons.qrc -o $MARKET_ENGINE_CLIENT_APPLICATION/src/third_party_icons_rc.py
