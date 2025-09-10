3d01d5d773838a2c1a778b1d83a659e6b629157b
1FIL0 <serglinux2@gmail.com>
2025-09-10 11:41:19 +0100

    fix clear.sh

--- make/mk_clear.sh
+++ make/mk_clear.sh
@@ -11,9 +11,9 @@ echo ""
 rm -r $BUILD_PATH/* 2> /dev/null
 rm $APPIMG_CLIENT_PATH 2> /dev/null
 rm $APPIMG_API_PATH 2> /dev/null
-rm $APPIMG_CLIENT_DIR/bin/* 2> /dev/null
-rm $APPIMG_API_DIR/bin/* 2> /dev/null
-rm $APPIMG_CLIENT_DIR/usr/share/* 2> /dev/null
+rm -r $APPIMG_CLIENT_DIR/bin/* 2> /dev/null
+rm -r $APPIMG_API_DIR/bin/* 2> /dev/null
+rm -r $APPIMG_CLIENT_DIR/usr/lib/* 2> /dev/null
 rm -r $ZIPDIR_CLIENT_LINUX_X86_64/* 2> /dev/null
 rm -r $ZIPDIR_CLIENT_WINDOWS_X86_64/* 2> /dev/null
 rm -r $ZIPDIR_API_LINUX_X86_64/* 2> /dev/null
