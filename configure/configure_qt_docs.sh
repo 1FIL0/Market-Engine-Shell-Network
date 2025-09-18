193b683dacf14107cea2c0afae06d07109eca8fe
1FIL0 <serglinux2@gmail.com>
2025-09-18 01:44:48 +0100

    add licence headers

--- configure/configure_qt_docs.sh
+++ configure/configure_qt_docs.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+#* Market Engine Shell Network
+#* Copyright (C) 2025 OneFil (1FIL0) https://github.com/1FIL0
+#*
+#* This program is free software: you can redistribute it and/or modify
+#* it under the terms of the GNU General Public License as published by
+#* the Free Software Foundation, either version 3 of the License, or
+#* (at your option) any later version.
+#*
+#* This program is distributed in the hope that it will be useful,
+#* but WITHOUT ANY WARRANTY; without even the implied warranty of
+#* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#* GNU General Public License for more details.
+#*
+#* You should have received a copy of the GNU General Public License
+#* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#* See LICENCE file.
+
 HERE="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 UTILS_PATH=$HERE/../utils
 source $UTILS_PATH/definitions.sh
